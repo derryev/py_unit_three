36288d4b00a1999d43a997eb6b648c94d12c99a5
derryev <derryev@ssfs.org>
2023-10-05 11:29:32 -0400

    Completed addition.py.

--- addition.py
+++ addition.py
@@ -1,9 +1,14 @@
+# Eva D
 # Write your addition function in the space below...
-
+def addition(number_one, number_two):
+    total = number_one+number_two
+    print("Adding",number_one,"and",number_two,"makes a total of",total)
 
 # Do not change anything below these lines
 def main():
     pass
-
+    addition(1,2)
+    addition(3,4)
+    addition(5,6)
 if __name__ == '__main__':
     main()
