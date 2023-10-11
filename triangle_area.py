d07ba51d337ba65272adc43db2d87423aa3b6ff4
derryev <derryev@ssfs.org>
2023-10-11 08:45:50 -0400

    working on triangle_area.py

--- triangle_area.py
+++ triangle_area.py
@@ -1,2 +1,16 @@
+def calculate_area(a, b, c):
+    s = (a+b+c/2)
+    area = (s*(s-a)*(s-b)*(s-c))**0.5
+    return area
 
+def main():
+    side_a = 10
+    side_b = 20
+    side_c = 10
+    calculate_area(side_a, side_b, side_c)
+    print(calculate_area(side_a, side_b, side_c))
+
+
+
+main()
 
