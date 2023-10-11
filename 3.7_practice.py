1fad112d4b82be2151b6108b54e162013a460e3f
derryev <derryev@ssfs.org>
2023-10-11 08:28:36 -0400

    documenting code

--- 3.7_practice.py
+++ 3.7_practice.py
@@ -1,6 +1,12 @@
 
 
 def area_of_a_rectangle(length, height):
+    """
+    calculates the area of a rectangle
+    :param length: length of rectangle entered by the user
+    :param height: height of rectangle entered by the user
+    :return: the area of the rectanle
+    """
     area = length*height
     return area
 
