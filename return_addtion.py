d536dd79b21c75d022294e4407c78bf9f487c58a
derryev <derryev@ssfs.org>
2023-10-11 08:38:21 -0400

    Completed return_addition.py

---
+++ return_addtion.py
@@ -0,0 +1,18 @@
+def add_two(num1, num2):
+    """
+    adds two numbers together to get a sum
+    :param num1: the first number put in by the user
+    :param num2: the second number put in by the user
+    :return: the sum of two numbers
+    """
+    sum = num1+num2
+    return sum
+
+def main():
+    first_number = 345.56
+    second_number = 40
+    print(add_two(first_number, second_number))
+
+
+
+main()
