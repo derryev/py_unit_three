ed120faca8a7be8bdd0800f77d3b333b32484520
derryev <derryev@ssfs.org>
2023-10-16 15:16:24 -0400

    In progress assignment 3 and Unit testing.

--- Tests/assignment_three_tests.py
+++ Tests/assignment_three_tests.py
@@ -3,9 +3,24 @@ import assignment_three
 
 
 class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+    def test_surface_area_calculation(self):
+        self.assertEqual(184, assignment_three.calc_surface_area_of_prism(5,4,8))
 
+    def test_surface_area_calculation(self):
+        self.assertEqual(150, assignment_three.calc_surface_area_of_prism(4,3,9))
+
+    def test_final_surface_area_output(self):
+        import sys
+        from io import StringIO
+        saved_stdout = sys.stdout
+        try:
+            out = StringIO()
+            sys.stdout = out
+            assignment_three.print_answer(184)
+            output = out.getvalue().strip()
+            assert output == "The surface area of the prism is 184"
+        finally:
+            sys.standout = saved_stdout
 
 if __name__ == '__main__':
     unittest.main()
--- assignment_three.py
+++ assignment_three.py
@@ -1,29 +1,60 @@
 # Eva D
-# October {} 2023
+# 10/16/2023
 # takes input from user on length, width, and height of a prism to calculate its surface area
 
 def instructions():
+    """
+    Prints instructions on purpose of and how to use the program to the user.
+    """
     print("Welcome to the total surface area of a rectangular prism calculator!")
     print("This program computes the total surface area of a rectangular prism.")
     print("Please input the length, width, and height of the solid.")
 
 def get_length_input():
+    """
+    Gets user input on the length of the rectangular prism in the form of a string, converts input to a float.
+    :return: The length of the prism as a number(float).
+    """
     length = float(input("Please enter the length: "))
     return length
 
 def get_width_input():
+    """
+    Gets user input on the width of the rectangular prism in the form of a string, converts input to a float.
+    :return: The width of the prism as a number (float).
+    """
     width = float(input("Please enter the width: "))
     return width
 
 def get_height_input():
+    """
+    Gets user input on the height of the rectangular prism in the form of a string, converts input to a float.
+    :return: The height of the prism as a number(float).
+    """
     height = float(input("Please enter the height: "))
     return height
 
 def calc_area(side1, side2):
+    """
+    Calculates the area of one side of a rectangular prism (a rectangle) using two parameters made up of the length,
+    width, or height of the rectangular prism depending on which side's area is being calculated.
+    :param side1: One of the side lengths (either width, length, or height) that the user entered (float).
+    :param side2: A different side length (either width, length, or height) that the user entered (float).
+    :return: The area of one specific side of the rectangular prism based on the lengths used as parameters (float).
+    """
     area = side1*side2
     return area
 
 def calc_surface_area_of_prism(length, width, height):
+    """
+    Calculates the total surface area of the rectangular prism, using the length, width, and height input
+    by the user. Uses the calc_area function to find the area of each side and then adds the area of each side of the
+    prism together to get total surface area.
+    :param length: The length of the prism input by the user (float).
+    :param width: The width of the prism input by the user (float).
+    :param height: The height of the prism input by the user (float).
+    :return: The total surface area of the rectangular prism(float).
+    """
     top = calc_area(length, width)
     bottom = calc_area(length, width)
     left_side = calc_area(height, width)
@@ -34,6 +65,11 @@ def calc_surface_area_of_prism(length, width, height):
     return surface_area
 
 def print_answer(surface_area):
+    """
+    prints the total surface area of the rectangular prism back to the user with description of what is being printed.
+    :param surface_area: the surface area of the prism previously calculated in the calc_surface_area_of_prism function
+    (float)
+    """
     print("The surface area of the prism is", surface_area)
 
 
